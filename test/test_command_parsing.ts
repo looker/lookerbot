bc045b1ba63c16003c86f4dcff2293b2d6d44b80
Wil Gieseler <wil@wilgieseler.com>
2017-06-07 15:36:12 -0700

    tslint touches tests

--- test/test_command_parsing.ts
+++ test/test_command_parsing.ts
@@ -1,16 +1,15 @@
-import "mocha";
-import { SlackUtils } from '../lib/slack_utils';
-import { expect } from 'chai';
+import { expect } from "chai"
+import "mocha"
+import { SlackUtils } from "../lib/slack_utils"
 
 it("passes through basic text", () => {
-  expect(SlackUtils.stripMessageText("hello")).to.equal("hello");
-});
+  expect(SlackUtils.stripMessageText("hello")).to.equal("hello")
+})
 
 it("fixes gt", () => {
-  expect(SlackUtils.stripMessageText("coolcommand &gt; 50")).to.equal("coolcommand > 50");
-});
+  expect(SlackUtils.stripMessageText("coolcommand &gt; 50")).to.equal("coolcommand > 50")
+})
 
 it("fixes lt", () => {
-  expect(SlackUtils.stripMessageText("coolcommand &lt; 50")).to.equal("coolcommand < 50");
-});
-
+  expect(SlackUtils.stripMessageText("coolcommand &lt; 50")).to.equal("coolcommand < 50")
+})
